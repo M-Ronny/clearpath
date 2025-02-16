5fbd1b73457be40a7fe8c943431b0d16e0380736
Ronaldo <ronaldomorales.ai@gmail.com>
2025-02-16 14:28:15 -0500

    first commit

---
+++ src/shhh.js
@@ -0,0 +1,3 @@
+export const PROJECT_ID = import.meta.env.VITE_PROJECT_KEY;
+export const DATABASE_ID = import.meta.env.VITE_DATABASE_KEY;
+export const COLLECTION_ID = import.meta.env.VITE_COLLECTION_KEY;
