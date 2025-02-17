c4990dc6268294f4ce66905ca018f6734bfc08eb
Cristian <r.cristiano.ai@gmail.com>
2025-02-16 22:13:38 -0500

    Modified index.html, main.js, & shhh.js
    Tweaked two lines on index.html, added COLLECTION_ID & BUCKET_ID for shhh.js, and modified main.js to include image uploading and storing, display the description and image of post and cleaned up code.

--- src/main.js
+++ src/main.js
@@ -1,108 +1,103 @@
 import './style.css'
-import { PROJECT_ID, DATABASE_ID, COLLECTION_ID } from './shhh.js';
-
-import { Client, Databases, ID } from "appwrite";
+import { PROJECT_ID, DATABASE_ID, COLLECTION_ID, BUCKET_ID } from './shhh.js';
+import { Client, Databases, Storage, ID } from "appwrite";
 
 const client = new Client()
     .setEndpoint('https://cloud.appwrite.io/v1')
     .setProject(PROJECT_ID);
 
 const databases = new Databases(client);
+const storage = new Storage(client);
 
-const form = document.querySelector('form')
+const form = document.querySelector('form');
 
-form.addEventListener('submit', addPost)
+form.addEventListener('submit', addPost);
 
-function addPost(e){
-  e.preventDefault()
-  const post = databases.createDocument(
+async function addPost(e) {
+  e.preventDefault();
+
+  const description = e.target.description.value;
+  const imageFile = e.target.image.files[0];
+
+  if (!imageFile) {
+    alert("Please upload an image.");
+    return;
+  }
+
+  let imageUrl = "";
+
+  try {
+    const uploadedFile = await storage.createFile(
+      BUCKET_ID,
+      ID.unique(),
+      imageFile
+    );
+
+    const imageId = uploadedFile.$id;
+    imageUrl = `https://cloud.appwrite.io/v1/storage/buckets/${BUCKET_ID}/files/${imageId}/view?project=${PROJECT_ID}`;
+    console.log("Image uploaded:", imageUrl);
+  } catch (error) {
+    console.log("Error uploading image:", error);
+    alert("Image upload failed");
+    return;
+  }
+
+  const post = await databases.createDocument(
     DATABASE_ID,
     COLLECTION_ID,
     ID.unique(),
-    { "description": e.target.description.value,
-      // "image":  e.target.image.value,
-      "dermatologist-recommendation": e.target.value
-     }
+    { 
+      "description": description,
+      "image": imageUrl
+    }
   );
-  post.then(function (response) {
-      addPostsToDom()
-  }, function (error) {
-      console.log(error);
+
+  console.log("Post created:", post);
+  addPostsToDom();
+  form.reset();
+}
+
+async function addPostsToDom() {
+  document.querySelector('ul').innerHTML = "";
+  let response = await databases.listDocuments(DATABASE_ID, COLLECTION_ID);
+
+  response.documents.forEach((post) => {
+    const li = document.createElement('li');
+    li.innerHTML = `
+      <p>${post['description']}</p>
+      <img src="${post.image}" alt="Image" style="width: 200px;">
+      <p>Has it been answered? ${post.answered ? post.answered : "No"}</p>
+    `;
+    li.id = post.$id;
+
+    const deleteBtn = document.createElement('button');
+    deleteBtn.textContent = '🧨';
+    deleteBtn.onclick = () => removePost(post.$id);
+
+    const coffeeBtn = document.createElement('button');
+    coffeeBtn.textContent = '✔️';
+    coffeeBtn.onclick = () => updateAnswer(post.$id);
+
+    li.appendChild(coffeeBtn);
+    li.appendChild(deleteBtn);
+    document.querySelector('ul').appendChild(li);
   });
-  form.reset()
-}
 
-async function addPostsToDom(){
-    document.querySelector('ul').innerHTML = ""
-    let response = await databases.listDocuments(
+  async function removePost(id) {
+    await databases.deleteDocument(DATABASE_ID, COLLECTION_ID, id);
+    document.getElementById(id).remove();
+  }
+
+  async function updateAnswer(id) {
+    databases.updateDocument(
       DATABASE_ID,
-      COLLECTION_ID
-  );
-  //console.log(response.documents[0])
-  response.documents.forEach((post)=>{
-    const li = document.createElement('li')
-    li.textContent = `${post['description']} ${post['image']} Has it been answered? ${post['answered']} `
-
-    li.id = post.$id
-
-    const deleteBtn = document.createElement('button')
-    deleteBtn.textContent = '🧨'
-    deleteBtn.onclick = () => removePost(post.$id)
-
-    const coffeeBtn = document.createElement('button')
-    coffeeBtn.textContent = '✔️'
-    coffeeBtn.onclick = () => updateAnswer(post.$id)
-
-    li.appendChild(coffeeBtn)
-    li.appendChild(deleteBtn)
-
-    document.querySelector('ul').appendChild(li)
-  })
-
-  async function removePost(id){
-    const result = await databases.deleteDocument(
-      DATABASE_ID, // databaseId
-      COLLECTION_ID, // collectionId
-      id // documentId
-    );
-    document.getElementById(id).remove()
-  
+      COLLECTION_ID,
+      id,
+      { 'answered': true }
+    ).then(() => {
+      location.reload();
+    });
   }
-  async function updateAnswer(id){
-    const result = databases.updateDocument(
-      DATABASE_ID, // databaseId
-      COLLECTION_ID, // collectionId
-      id, // documentId
-      {'answered': true} // data (optional)
-        // permissions (optional)
-    );
-    result.then(function(){location.reload()})
-  }
-
-  // promise.then(function (response) {
-  //     console.log(response);
-  // }, function (error) {
-  //     console.log(error);
-  // });
 }
-addPostsToDom()
 
-// const promise = databases.createDocument(
-//     DATABASE_ID,
-//     COLLECTION_ID,
-//     ID.unique(),
-//     { "company-name": "100Devs",
-//       "date-added": new Date(),
-//       "role": "software engineer",
-//       "location": "Philly",
-//       "position-type": "full time",
-//       "source": "https://100devs.org"
-//      }
-// );
-
-// promise.then(function (response) {
-//     console.log(response);
-// }, function (error) {
-//     console.log(error);
-// });
-
+addPostsToDom();
